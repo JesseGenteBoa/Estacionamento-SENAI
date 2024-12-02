4545feebaec9d4d9d23d491987147d6194d9f888
JesseGenteBoa <153686378+JesseGenteBoa@users.noreply.github.com>
2024-12-02 00:49:50 -0300

    Update veiculo.py

--- veiculo.py
+++ veiculo.py
@@ -1,30 +1,27 @@
 
-class Veiculo:
+class GerenciadorVeiculos:
 
     def __init__(self):
         self.placas_precadastradas = {
         'ABC1D23': ['Moto', 'Honda', '2017', 'Vermelho', 'Gabriel Brito'],
         'EFG4H56': ['Moto', 'Honda', '2015', 'Preto', 'Ednaldo Pereira'],
-        'IJK7891': ['Moto', 'Yamaha', '2022', 'Azul Escuro', 'Pedro Aureliano Buendía'],
+        'IJK7891': ['Moto', 'Yamaha', '2022', 'Azul Escuro', 'Aureliano Buendía'],
         'LMN7258': ['Carro', 'Chevrolet', '2017', 'Cinza', 'Harrison Ford'],
-        'ABC4F78': ['Carro', 'Fiat', '2007', 'Preto', 'Jessé Ramos'],
-        'FGV8G41': ['Carro', 'Honda', '2014', 'Preto', 'Jeronimo Einstein'],
+        'ABC4F78': ['Carro', 'Fiat', '2007', 'Preto', 'Jessé Lineu Silva'],
+        #'FGV8G41': ['Carro', 'Honda', '2014', 'Preto', 'Jeronimo Einstein'],
         'GPT1W45': ['Carro', 'Peogeut', '2019', 'Azul', 'Esdras da Silva Sauro']
         }
 
 
     def cadastrar(self, placa, lista_de_atributos):
         self.placas_precadastradas[placa] = lista_de_atributos
-        
-
-    def imprimir_cadastrados(self, placa):    
         print(self.placas_precadastradas[placa])
-        #return self.placas_precadastradas[placa]
+        print(self.placas_precadastradas)
 
 
     def verificar_cadastrados(self, detected_texts):
         try:
             _ = self.placas_precadastradas[detected_texts]
-            return True
+            return self.placas_precadastradas[detected_texts]
         except:
-             return False
+             return []
